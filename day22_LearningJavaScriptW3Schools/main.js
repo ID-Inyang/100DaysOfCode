2ba49dea73c335331e8a8a3911fd1db217c50331
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-10-12 23:59:21 +0100

    day 22

---
+++ day22_LearningJavaScriptW3Schools/main.js
@@ -0,0 +1,57 @@
+//Constant Arrays
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+cars[0] = "Hyundai";
+
+cars.push("Honda");
+
+// cars = ['Mitsubushi', 'Ford', 'Lexus'];
+
+/*console.log(
+    typeof 0,
+    typeof 314,            
+    typeof 3.14 ,          
+    typeof (3),            
+    typeof (3 + 4)
+);
+
+let a = 3;
+// let x = (100 + 50) * a;
+// console.log(x);
+
+let text1 = 'A';
+let text2 = 'a';
+let result = text1 < text2;
+
+let x = 5;
+let y = 2;
+result = x % y;
+
+result = x ** y;
+result = Math.pow(a, y);
+console.log(result);*/
+
+// console.log(100 + 50 - 3);
+
+// let x = 1;
+// let y = x &&= 10;
+// console.log(y);
+
+// let x = 60;
+// x ??= 10;
+// console.log(x);
+
+// let text = "12345";
+
+// let min = Math.min(...text);
+// let max = Math.max(...text);
+
+// console.log(min);
+// console.log(max);
+
+let text1 = "20";
+let text2 = "5";
+let result = text1 < text2;
+
+console.log(resultg);
