b89e286d886052fbade461663082b859d8e3337f
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-10-19 20:53:39 +0100

    Day 29 - JavaScript Numbers

--- main.js
+++ main.js
@@ -55,7 +55,43 @@ x = 9.656;
 // console.log(x.toExponential());
 
 x = 9.656;
-console.log(x.toFixed(0));
-console.log(x.toFixed(2));
-console.log(x.toFixed(4));
-console.log(x.toFixed(6));
+// console.log(x.toFixed(0));
+// console.log(x.toFixed(2));
+// console.log(x.toFixed(4));
+// console.log(x.toFixed(6));
+
+// console.log(x.toPrecision());
+// console.log(x.toPrecision(2));
+// console.log(x.toPrecision(4));
+// console.log(x.toPrecision(6));
+
+x = 123;
+// console.log(x.valueOf());
+// console.log((123).valueOf());
+// console.log((100 + 23).valueOf());
+
+// console.log(Number(true));
+// console.log(Number("10.25"));
+// console.log(Number(new Date("1970-01-01")));
+
+// console.log(parseInt("-10"));
+// console.log(parseInt("10 20 30"));
+// console.log(parseInt("c 10 years"));
+
+
+
+// console.log(Number.isInteger(10));
+// console.log(Number.isInteger(10.5));
+// console.log("8hp")
+
+// x = Number.NEGATIVE_INFINITY;
+// console.log(x)
+
+// console.log(5 & 1);
+// console.log(~5);
+// console.log(5 << 1);
+// console.log(5^1);
+x = 5;
+y = 1;
+
+console.log(x <<= y);
