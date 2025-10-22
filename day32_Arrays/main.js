6dff5fc9d3b13a89aca82762c2032d3dab05d3d9
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-10-22 21:32:36 +0100

    Day 32 - Arrays

---
+++ day32_Arrays/main.js
@@ -0,0 +1,19 @@
+const fruits = ["Banana", "Orange", "Apple"];
+fruits[6] = "Lemon";
+
+// console.log(fruits);
+
+const person = [];
+person["firstName"] = "John";
+person["lastName"] = "Doe";
+person["age"] = 46;
+// console.log(person.length);     // Will return 0
+// console.log(person[0]); 
+
+const points = new Array(10);
+console.log("..........")
+// console.log(Array.isArray(points));
+// console.log(fruits instanceof Array);
+
+// fruits.length = 1;
+console.log(fruits[-1]);
