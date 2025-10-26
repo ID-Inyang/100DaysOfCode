7c82f5fe7892e1d3dc1175dac61c0f4a46fc491b
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-10-26 22:32:09 +0100

    Day 36

--- day35_TypedArrays/main.js
+++ day35_TypedArrays/main.js
@@ -11,9 +11,38 @@ const myArr3 = Int16Array.from("1234567890");
 // console.log(myArr.BYTES_PER_ELEMENT);
 
 myArr.fill(387);
-console.log(myArr);
+// console.log(myArr);
 
 const letters = new Set(["a","b","c"]);
 
 // Create an Iterator
-const myIterator = letters.values();
+const myIterator = letters.entries();
+// console.log(myIterator);
+
+let text = "";
+
+for (const entry of myIterator) {
+    // console.log(entry);
+    continue;
+}
+
+const A = new Set(['a','b','c']);
+const B = new Set(['b','c','d']);
+
+const C = A.intersection(B);
+
+// console.log(C)
+
+//Set
+let mySet = new Set();
+
+let myObj = {fname:"ID.", lname:"Inyang"};
+
+mySet.add(myObj);
+
+console.log(mySet);
+
+myObj = null
+mySet.add(myObj)
+
+console.log(mySet);
